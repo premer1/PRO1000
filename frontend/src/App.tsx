4c5cc2ed23142be61ca9d75e8a9a2a4628a2e516
premer1 <premer1@outlook.com>
2026-01-28 23:00:57 +0100

    test2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
     <div className="h-screen flex items-center justify-center bg-slate-900">
           <h1 className="text-5xl font-black text-emerald-400 drop-shadow-lg">
             Tailwind fungerer!
-            Oblidi oblada
           </h1>
         </div>
     </>
