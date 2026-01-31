7fab53de476f8fe0aca2c1b99b2beeaf05e681d5
OlaWold <ola@wold.bz>
2026-01-31 21:08:40 +0100

    Backend

--- src/main/java/com/example/crmproject/Tickets/TicketController.java
+++ src/main/java/com/example/crmproject/Tickets/TicketController.java
@@ -1,4 +1,22 @@
 package com.example.crmproject.Tickets;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/tickets")
 public class TicketController {
+    private final TicketsService service;
+
+    public TicketController(TicketsService service) {this.service = service; }
+
+    @GetMapping
+    public List<Tickets> getTickets() {return service.getAll(); }
+
+    @PostMapping
+    public Tickets create(@RequestBody Tickets tickets) { return service.create(tickets); }
+
+    @GetMapping("/{ticketsno}")
+    public Tickets byTickets(@PathVariable long ticketsno) { return service.getByTicketNo(ticketsno); }
 }
