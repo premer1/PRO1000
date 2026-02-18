8eb17dbccdde5c0882cb9bc767a5204dc512e771
OlaWold <ola@wold.bz>
2026-02-18 19:01:59 +0100

    shadcn

--- frontend/src/pages/DashboardT.tsx
+++ frontend/src/pages/DashboardT.tsx
@@ -1,12 +1,17 @@
+import { Button } from "../components/ui/button.tsx";
+
 const DashboardT = () => {
     return (
-
+<>
         <header className="flex flex-col items-center justify-center font-bold text-3xl">
             Dashboard
         </header>
+            <Button>Hei</Button>
 
 
 
+
+</>
     )
 };
 
