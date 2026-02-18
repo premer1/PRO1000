98a66034438ce5574cd67888fa1b57103a420014
OlaWold <ola@wold.bz>
2026-02-18 19:17:11 +0100

    shadcn1

--- frontend/src/pages/DashboardT.tsx
+++ frontend/src/pages/DashboardT.tsx
@@ -6,7 +6,7 @@ const DashboardT = () => {
         <header className="flex flex-col items-center justify-center font-bold text-3xl">
             Dashboard
         </header>
-            <Button>Hei</Button>
+            <Button className="rounded-4xl p-10 rotate-302">Hei</Button>
 
 
 
